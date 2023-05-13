38b2dfc534b82c401eac476ce841b4b3edd5d45f
hee <cmlgml094356@gmail.com>
2023-05-13 19:27:48 +0900

    [setting] project setting

---
+++ src/main/java/com/dsm/newtrashbackspringboot/NewTrashBackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.dsm.newtrashbackspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewTrashBackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewTrashBackSpringBootApplication.class, args);
+	}
+
+}
