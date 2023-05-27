ccca3be1bec8e8a06e70b7ba94ec998cef26685c
hee <cmlgml094356@gmail.com>
2023-05-27 23:04:02 +0900

    fix : repository 수정

--- src/main/java/com/dsm/newtrash/back/springboot/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/dsm/newtrash/back/springboot/domain/user/domain/repository/UserRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.dsm.newtrash.back.springboot.domain.user.domain.User;
 
-public interface UserRepository extends CrudRepository<String, User> {
+public interface UserRepository extends CrudRepository<User, String> {
 
 }
