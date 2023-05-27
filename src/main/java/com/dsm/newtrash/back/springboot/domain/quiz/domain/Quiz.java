29582ac6fb9bc45ae0c6fa59db830c79c9f0b147
hee <cmlgml094356@gmail.com>
2023-05-27 23:12:26 +0900

    update : quiz와 user 연관관계 매핑

--- src/main/java/com/dsm/newtrash/back/springboot/domain/quiz/domain/Quiz.java
+++ src/main/java/com/dsm/newtrash/back/springboot/domain/quiz/domain/Quiz.java
@@ -1,12 +1,16 @@
 package com.dsm.newtrash.back.springboot.domain.quiz.domain;
 
 import com.dsm.newtrash.back.springboot.domain.quiz.domain.type.CategoryType;
+import com.dsm.newtrash.back.springboot.domain.user.domain.User;
 import com.dsm.newtrash.back.springboot.global.Entity.BaseIdEntity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -31,13 +35,18 @@ public class Quiz extends BaseIdEntity {
 	@Enumerated(EnumType.STRING)
 	private CategoryType category;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
 
 	@Builder
-	private Quiz(String title, String introduction, String path, CategoryType category) {
+	private Quiz(String title, String introduction, String path, CategoryType category, User user) {
 		this.title = title;
 		this.introduction = introduction;
 		this.path = path;
 		this.category = category;
+		this.user = user;
 	}
 
 }
