7b3abd680fa53ad85a7fc8372e129b53c8eb66eb
hee <cmlgml094356@gmail.com>
2023-05-27 23:05:12 +0900

    update : jpaRepositroy로 수정

--- src/main/java/com/dsm/newtrash/back/springboot/domain/problem/domain/repository/AnswerRepository.java
+++ src/main/java/com/dsm/newtrash/back/springboot/domain/problem/domain/repository/AnswerRepository.java
@@ -1,9 +1,9 @@
 package com.dsm.newtrash.back.springboot.domain.problem.domain.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dsm.newtrash.back.springboot.domain.problem.domain.Answer;
 
-public interface AnswerRepository extends CrudRepository<Answer, Long> {
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
 
 }
