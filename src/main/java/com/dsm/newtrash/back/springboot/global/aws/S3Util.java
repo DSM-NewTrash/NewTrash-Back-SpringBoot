27c34d303bc82868c4868315de93d71b25fbd4cc
hee <cmlgml094356@gmail.com>
2023-05-29 21:15:20 +0900

    create : s3 설정

--- src/main/java/com/dsm/newtrash/back/springboot/global/aws/S3Util.java
+++ src/main/java/com/dsm/newtrash/back/springboot/global/aws/S3Util.java
@@ -1,19 +1,31 @@
-package com.dsm.newtrash.back.springboot.global.util;
+package com.dsm.newtrash.back.springboot.global.aws;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.amazonaws.services.s3.AmazonS3;
 
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
-public class ImageUtil {
+public class S3Util {
+
+	private final AmazonS3 amazonS3;
+
+	@Value("${s3.bucket}")
+	private String bucketName;
 
 	@Value("${s3.default.image.quiz}")
 	private String quizDefaultImage;
 
 	@Value("${s3.default.image.problem}")
 	private String problemDefaultImage;
+
+
+	public void delete(String objectName) {
+		amazonS3.deleteObject(bucketName, objectName);
+	}
 
 	public String getQuizDefaultImage(String image) {
 		return (image.isEmpty()) ? quizDefaultImage : image;
