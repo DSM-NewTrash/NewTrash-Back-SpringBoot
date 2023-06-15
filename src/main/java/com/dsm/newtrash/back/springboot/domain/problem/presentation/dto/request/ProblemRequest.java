51b1985a59dd50a21db2e7dc942cf236e3eafc34
hee <cmlgml094356@gmail.com>
2023-06-15 15:05:26 +0900

    update : null 이 통과하지 못하도록 변경

--- src/main/java/com/dsm/newtrash/back/springboot/domain/problem/presentation/dto/request/ProblemRequest.java
+++ src/main/java/com/dsm/newtrash/back/springboot/domain/problem/presentation/dto/request/ProblemRequest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 
@@ -19,7 +20,8 @@ public class ProblemRequest {
 	private String question;
 
 	@Max(value = 5)
-	private int correctAnswer;
+	@NotNull(message = "입력해주세요.")
+	private Integer correctAnswer;
 
 	@NotBlank(message = "입력해주세요.")
 	@Size(max = 50, message = "50자 이하로 입력해주세요.")
